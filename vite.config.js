3bffe42abb93600c74d8de4ca3493d32684b7faa
idrodrigo <idrodrigo@icloud.com>
2023-07-09 19:37:44 -0600

    rm base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: 'https://idrodrigo.github.io/react-contacts/'
+  plugins: [react()]
 })
